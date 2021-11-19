369e3df01b680c8764f77577d294aaf77deaaed6
logan-r <logan.ralston@mail.mcgill.ca>
2021-11-19 15:50:17 -0500

    refactor: move border into FAQ

--- src/components/Sponsor/Container.jsx
+++ src/components/Sponsor/Container.jsx
@@ -2,7 +2,6 @@ import styled from "styled-components"
 import * as styleVars from "../variable"
 
 export const Container = styled.section`
-  margin-bottom: 120px;
   background-color: ${styleVars.darkSecondaryBlue};
 `
 
