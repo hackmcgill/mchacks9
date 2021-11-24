690229ebb7ed260c47c23d4a18affa364018b902
Anmol Brar <brarsanmol@protonmail.com>
2021-11-24 13:51:43 -0500

    fix: use navigate function properly.
    - Call navigate on useEffect so it is triggered on page load.
    - Return empty page to satisfy react requirements.

--- src/components/404/404.jsx
+++ src/components/404/404.jsx
@@ -1,7 +1,13 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { navigate } from "gatsby"
 
 // (2021-11-24) - The navigate function will be removed in the next major release of Gatsby.
-const NotFound = () => navigate("/")
+const NotFound = () => {
+    useEffect(() => navigate("/"))
+    return (
+        <>
+        </>
+    )
+}
 
 export default NotFound
