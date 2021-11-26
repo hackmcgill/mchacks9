b4f861be6b003fee04e2df06b0516edb6d4e1c6e
Anmol Brar <brarsanmol@protonmail.com>
2021-11-26 18:20:46 -0500

    fix: update colour name for footer.

--- src/components/Footer/Container.jsx
+++ src/components/Footer/Container.jsx
@@ -3,7 +3,7 @@ import * as styleVars from "../variable"
 
 export const Container = styled.footer`
   padding: 2rem 0;
-  border-top: 2px solid ${styleVars.backgroundGradientStart};
+  border-top: 2px solid ${styleVars.lightSecondaryBlue};
   display: flex;
   width: 100%;
   justify-content: space-between;
