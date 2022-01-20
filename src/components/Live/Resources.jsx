18ebaf07b4cb0f6c9d79ee0c04861394507d170c
Yun Kai Peng <yunkaipeng0129@gmail.com>
2022-01-20 16:40:12 -0500

    Add sponsor api

--- src/components/Live/Resources.jsx
+++ src/components/Live/Resources.jsx
@@ -8,121 +8,63 @@ const Resources = ({ visible }) => (
   <LiveContainer className={!visible ? "hidden" : ""}>
     <h2>Sponsor Resources</h2>
     <QuestionGroup>
-    <Question question="Balsamiq API">
-      Balsamiq Wireframes is the perfect first UX design tool for anyone
-      interested in getting into UX. It's by far the most approachable: you just
-      drag, drop, and arrange controls on the page, and voila', you made your
-      first User Interface! They even have online courses and articles to teach
-      you all about UI design.
+    <Question question="OVHCloud">
+    Create your first OVHcloud public cloud project! 
+    Learn how to create your first Public Cloud project via the OVHcloud Control Panel.
       <br />
-      Learn more{" "}
+      Learn more {" "}
       <a
-        href="https://balsamiq.com/wireframes/cloud/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        here
-      </a>
-      .
+          href="https://docs.ovh.com/gb/en/public-cloud/create_a_public_cloud_project/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          here
+        </a>
+        .
+      <br/>
+      Voucher Code: OVHcloud-McHacks
     </Question>
 
-    <Question question="Twilio API">
-      Facilitate your app's communication functions with the Twilio API found{" "}
+    <Question question="Balsamiq">
+      Get a 90-day extended trial promo code when using Balsamiq Cloud!
+      <br />
+      Learn more about Balsamiq{" "}
       <a
-        href="https://www.twilio.com/blog/apply-promo-code"
+        href="https://balsamiq.cloud/"
         target="_blank"
         rel="noopener noreferrer"
       >
         here
       </a>
       .
+      <br/>
+      To use their API, follow the steps{" "}
+      <a
+        href="https://balsamiq.com/wireframes/cloud/sales/promo/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        here
+      </a>
+      .
+      <br/>
+      Code: BQRSDEHW010522
     </Question>
 
-    <Question question="echoAR API">
-      echoAR is a cloud platform for augmented and virtual reality (AR/VR) that
-      provides tools and server-side infrastructure to help developers &
-      companies quickly build and deploy AR/VR apps and experience.
+    <Question question="Twilio">
+      Facilitate your app’s communication functions and design system using the Twilio API as well as their design system.
       <br />
-      Learn more{" "}
+      Learn more {" "}
       <a
-        href="https://console.echoar.xyz/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        here
-      </a>
-      .
-    </Question>
-
-    <Question question="Big Bang API">
-      This prediction Service from Salesforce is one of Big Bang's core
-      products!
-      <br />
-      Learn more{" "}
-      <a
-        href="https://help.salesforce.com/articleView?id=bi_edd_prediction_service.htm&type=5Einstein"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        here
-      </a>
-      .
-      <br />
-      The API resource for Big Bang's productivity challenge can also be found{" "}
-      <a
-        href="https://developers.google.com/drive/api/v3/reference"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        here
-      </a>
-      .
-    </Question>
-
-    <Question question="Business-in-a-Box API">
-      Over 2,000 high-quality fill-in the blanks templates created by business
-      experts & lawyers. Get your business-in-a-box toolkit. Save money on legal
-      fees. Build your dream business. Save your time & money.
-      <br />
-      Learn more{" "}
-      <a
-        href="https://app.business-in-a-box.com/account/create"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        here
-      </a>
-      .
-    </Question>
-
-    <Question question="Voiceflow API">
-      Visually design, prototype and publish Alex Skills and Google Actions
-      without writing code!
-      <br />
-      Learn more{" "}
-      <a
-        href="https://www.voiceflow.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        here
-      </a>
-      .
-    </Question>
-
-    <Question question="The Codex">
-      Jumpstart your programming journey with The Codex's engaging projects.
-      Clear, concise, and practical videos with no fluff!
-      <br />
-      Get started{" "}
-      <a
-        href="https://thecodex.me/app/get-started"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        here
-      </a>
-      .
+          href="https://www.twilio.com/blog/apply-promo-code"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          here
+        </a>
+        .
+      <br/>
+      Promo code: MCHACKS22
     </Question>
     </QuestionGroup>
   </LiveContainer>
